4203aefdcbffc1ea945e5c247b44d0e561e5156e
Albert <albertbosch81@gmail.com>
2023-03-15 21:05:49 +0100

    lessons 26 27

---
+++ exercices/lessons26_27/src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+import TaskListContainer from "./components/containers/taskListContainer";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <TaskListContainer/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
